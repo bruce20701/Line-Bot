7aabb7b6bc9e5030f25507ec2e3c2288fecba39a
flystar0526 <flowermetoer@gmail.com>
2023-06-02 20:37:40 +0800

    debug

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  line_bot_api = LineBotApi('你的 access token')
+  line_bot_api = LineBotApi('c/2axBtAYZQkLSDHOVVnBLXdJLs0RabigC1tvKtEbNb8/8P6f14yLRrfAQKKiUH6KgcdQp+bDHEyI7qP0/zHpvJflIKqIFRl/l2fbVvQS3txUy1LcNpHR/iCIjjmshTzt5zQQrjEGbsLx2MorQ6CcAdB04t89/1O/w1cDnyilFU=')
   try:
     # 網址被執行時，等同使用 GET 方法發送 request，觸發 LINE Message API 的 push_message 方法
-    line_bot_api.push_message('你的 User ID', TextSendMessage(text='Hello World!!!'))
+    line_bot_api.push_message('U3b706ee724da7f1ccaf51c2fb357d507', TextSendMessage(text='Hello World!!!'))
     return 'OK'
   except:
     print('error')
