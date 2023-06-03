e8ba16826cf1ffb91f84ee0e96dbb1afa5a00349
flystar0526 <flowermetoer@gmail.com>
2023-06-03 14:42:14 +0800

    add 1A2B Game

--- app.py
+++ app.py
@@ -3,15 +3,22 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage
 from model import Course, LinkedList
 import threading
+import json
 import time
 import datetime
+import random
 import pytz
 import csv
 
 app = Flask(__name__)
 
-#line_bot的Token
+#line_bot的Token、Channel Secret
 line_bot_api = LineBotApi('c/2axBtAYZQkLSDHOVVnBLXdJLs0RabigC1tvKtEbNb8/8P6f14yLRrfAQKKiUH6KgcdQp+bDHEyI7qP0/zHpvJflIKqIFRl/l2fbVvQS3txUy1LcNpHR/iCIjjmshTzt5zQQrjEGbsLx2MorQ6CcAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('124fa4bcf00b1166c14eeea6bc6f2d00')
+
+# 1A2B遊戲
+ansNum = []
+gameMod = False
 
 # 初始化鏈結
 headNode = None
@@ -70,10 +77,10 @@ def checkTime():
       nodeTime = datetime.datetime.strptime(tempNode.startTime, "%H:%M")
       nodeHour = nodeTime.hour
       nodeMinute = nodeTime.minute
-      print(f"鏈結時間： Week: {tempNode.day} Hour: {nodeHour} Minute: {nodeMinute}")
+      print(f"課程時間： Week: {tempNode.day} Hour: {nodeHour} Minute: {nodeMinute}")
       print()
       #判斷通知時間
-      if int(curWeekday) == int(tempNode.day) and (nodeHour * 60 + nodeMinute) - (curHour * 60 + curMinute) == 0:
+      if int(curWeekday) == int(tempNode.day) and (nodeHour * 60 + nodeMinute) - (curHour * 60 + curMinute) == 30:
          messageStr = f"課程通知\n課程名稱：{tempNode.name}\n教室：{tempNode.location}\n上課時間：{tempNode.startTime}\n下課時間：{tempNode.endTime}\n祝您上課愉快！"
          line_bot_api.push_message('U3b706ee724da7f1ccaf51c2fb357d507', TextSendMessage(text=messageStr))
          #陣列指標往後移
@@ -83,16 +90,74 @@ def checkTime():
       # 系統休息10秒
       time.sleep(10)
 
-# 接受使用者訊息(還沒動工)
-@app.route("/")
-def home():
-   global line_bot_api
+# 查詢天氣
+def checkWeather():
+   pass
+
+# 1A2B遊戲
+def playGame(msg):
+   global ansNum, gameMod
+   numList = []
+   countA = 0
+   countB = 0
    try:
-      # 網址被執行時，等同使用 GET 方法發送 request，觸發 LINE Message API 的 push_message 方法
-      line_bot_api.push_message('U3b706ee724da7f1ccaf51c2fb357d507', TextSendMessage(text='你好!'))
-      return 'OK'
+      for i in range(len(msg)):
+         numList.append(int(msg[i]))
+      for i in range(max(len(msg), len(ansNum))):
+         if numList[i] == ansNum[i]:
+            countA += 1
+         elif numList[i] in ansNum:
+            countB += 1
+      if countA == 4:
+         message = f"恭喜答對，答案是{msg}\n要再次遊玩的話，請重新輸入「1A2B遊戲」"
+         gameMod = False
+      else:
+         message = f"{countA}A{countB}B"
+   except:
+      message = "輸入格式有誤，請重新輸入"
+   print(f"輸出結果:{message}")
+   return message
+
+# 初始化1A2B遊戲數字
+def initGame():
+   global ansNum
+   # 產生4個0~9之間的數，數字不會重複
+   ansNum = random.sample(range(10), 4)
+   print(f"答案：{ansNum}")
+
+# 接受使用者訊息
+@app.route("/", methods=['POST'])
+def linebot():
+   global line_bot_api, handler, gameMod
+   body = request.get_data(as_text=True)
+   json_data = json.loads(body)
+   try:
+      signature = request.headers['X-Line-Signature']
+      handler.handle(body, signature)
+      # 讀取回應Token
+      replyTk = json_data['events'][0]['replyToken']
+      # 讀取使用者輸入內容
+      userMsg = json_data['events'][0]['message']['text']
+      if gameMod:
+         if userMsg == "退出":
+            gameMod = False
+            message = "遊戲退出"
+            line_bot_api.reply_message(replyTk, TextSendMessage(text=message))
+         else:
+            message = playGame(userMsg)
+            line_bot_api.reply_message(replyTk, TextSendMessage(text=message))
+      else:
+         # 判斷使用者輸入內容
+         if userMsg == "查詢天氣":
+            message = checkWeather()
+         elif userMsg == "1A2B遊戲":
+            gameMod = True
+            initGame()
+            message = "1A2B遊戲開始\n請輸入4個數字\n如果要退出的話請輸入「退出」"
+            line_bot_api.reply_message(replyTk, TextSendMessage(text=message))
    except:
       print('error')
+   return 'OK'
 
 if __name__ == "__main__":
    # 定期偵測時間，每秒檢查一次
--- model.py
+++ model.py
@@ -10,3 +10,16 @@ class LinkedList:
     def __init__(self, value) -> None:
         self.value = value
         self.next = None
+
+class Weather:
+    def __init__(self, wx, maxT, minT, ci, pop) -> None:
+        # 天氣現象
+        self.wx = wx
+        # 最高溫
+        self.maxT = maxT
+        # 最低溫
+        self.minT = minT
+        # 舒適度
+        self.ci = ci
+        # 降雨機率
+        self.pop = pop
